098917a476c50e46f28c4c06d473d2147c9f3bae
Jhon Nieves <jancx031@gmail.com>
2021-11-17 13:03:01 -0400

    passing accountbalance state to accountbalance component through home

--- src/App.js
+++ src/App.js
@@ -4,15 +4,30 @@ import {Switch, Route} from 'react-router-dom';
 import Home from './components/Home';
 
 class App extends Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      accountBalance: 14568.27,
+    };
+  }
+
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path='/' render={(props) => <Home {...props} />} />
+          <Route exact path='/' render={() => <Home accountBalance={this.state.accountBalance} />} />
           <Route exact path='/login' render={() => <h1> Login Route </h1>} />
-          <Route exact path="/userProfile" render={() => <h1> User Profile Route </h1>}/>
-          <Route exact path="/accountbalance" render={() => <h1> Account Balance Route </h1>}/>
-          
+          <Route
+            exact
+            path='/userProfile'
+            render={() => <h1> User Profile Route </h1>}
+          />
+          <Route
+            exact
+            path='/accountbalance'
+            render={() => <h1> Account Balance Route </h1>}
+          />
         </Switch>
       </Router>
     );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,19 @@
-    // src/components/Home.js
-    
-    import React, { Component } from 'react';
-    
-    class Home extends Component {
-      render() {
-        return (
-            <div>
-              <img src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png" alt="bank"/>
-              <h1>Bank of React</h1>
-            </div>
-        );
-      }
-    }
-    
-    export default Home;
+// src/components/Home.js
+
+import React, {Component} from 'react';
+import AccountBalance from './AccountBalance';
+
+class Home extends Component {
+  render() {
+      
+    return (
+        <div>
+          <img src="https://picsum.photos/201" alt="bank"/>
+          <h1>Bank of React</h1>
+          <AccountBalance accountBalance={this.props.accountBalance}/>
+        </div>
+    );
+  }
+}
+
+export default Home;
